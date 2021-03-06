1cb54d5bfbe7b9f9cbde8867f609398707f7cd6d
newmarwegner <newmarwegner@gmail.com>
2021-03-06 12:05:29 -0300

    second commit

---
+++ script_to_run/dev_install.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/bash
+
+echo '######## Installing Python 3.9.0 and set as global #######'
+echo '##########################################################'
+echo '     ###############################################'
+username=$(logname)
+pyenv install 3.9.0
+pyenv global 3.9.0
+
+echo '### Adding function to bashrc to automate venv activate ###'
+echo '##########################################################'
+echo '     ###############################################'
+#Setting auto activate virtualenv when enter inside of directory
+## credits to https://gist.github.com/codysoyland/2198913
+cat >>/home/$username/.bashrc << 'EOF'
+_virtualenv_auto_activate() {
+    if [ -e ".venv" ]; then
+         Check to see if already activated to avoid redundant activating
+        if [ "$VIRTUAL_ENV" != "$(pwd -P)/.venv" ]; then
+            _VENV_NAME=$(basename `pwd`)
+            echo Activating virtualenv \"$_VENV_NAME\"...
+            VIRTUAL_ENV_DISABLE_PROMPT=1
+            source .venv/bin/activate
+            _OLD_VIRTUAL_PS1="$PS1"
+            PS1="($_VENV_NAME)$PS1"
+            export PS1
+        fi
+    fi
+}
+
+export PROMPT_COMMAND=_virtualenv_auto_activate
+EOF
+
+echo '## Creating a basic venv project and install useful sources with pip ##'
+echo '##########################################################'
+echo '     ###############################################'
+mkdir -m 760 /home/$username/Downloads/python_projects
+mkdir -m 760 /home/$username/Downloads/python_projects/basic_env
+cd /home/$username/Downloads/python_projects/basic_env/
+python -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install jupyter lab
+pip install pandas
+pip install geopandas
+pip install matplotlib
+pip install rasterio
+pip install Shapely
+
+echo "Setup DEV Finished, good luck and enjoy."
---
+++ script_to_run/gis_install.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/bash
+
+echo '### Update, Upgrading and Installing required packages ###'
+echo '##########################################################'
+echo '     ###############################################'
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev git
+
+echo '### ADD Repository in etc and Installing QGIS SOFTWARE ###'
+echo '##########################################################'
+echo '     ###############################################'
+
+echo " deb https://qgis.org/ubuntu-ltr $(lsb_release -s -c) main" >> /etc/apt/sources.list
+wget -O - https://qgis.org/downloads/qgis-2020.gpg.key | gpg --import
+gpg --fingerprint F7E06F06199EF2F2
+gpg --export --armor F7E06F06199EF2F2 | sudo gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/qgis-archive.gpg --import
+chmod a+r /etc/apt/trusted.gpg.d/qgis-archive.gpg
+sudo apt update
+sudo apt install qgis qgis-plugin-grass -y
+
+echo '## Installing PostgreSQL Database and Postgis extension ##'
+echo '##########################################################'
+echo '     ###############################################'
+
+sudo apt install postgresql postgis -y
+sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'postgres';"
+
+echo '################# Installing PGADMIN4 ####################'
+echo '##########################################################'
+echo '     ###############################################'
+
+curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && sudo apt update'
+sudo apt install pgadmin4 -y
+
+echo '############ Installing Pycharm Communit #################'
+echo '##########################################################'
+echo '     ###############################################'
+
+sudo snap install pycharm-community --classic
+
+echo '# Installing Pyenv to allow install differents pythons versions #'
+echo '##########################################################'
+echo '     ###############################################'
+
+username=$(logname)
+echo 'export PATH="/home/'$username'/.pyenv/bin:$PATH"'>>/home/$username/.bashrc
+echo 'eval "$(pyenv init -)"'>>/home/$username/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"'>>/home/$username/.bashrc
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | sudo -u $username bash
+
+echo "Running DEV set up."
+gnome-terminal -e 'bash -c "./dev_install.sh; bash"'
